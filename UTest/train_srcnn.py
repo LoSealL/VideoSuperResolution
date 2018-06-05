5c4bc5b9e9f945b041654a5483dd92718edcbf68
Tang, Wenyi <twytwy12345@live.com>
2018-06-05 14:47:12 +0800

    fix utest errors

--- UTest/train_srcnn.py
+++ UTest/train_srcnn.py
@@ -5,13 +5,14 @@ from VSR.Models.Srcnn import SRCNN
 
 
 if __name__ == '__main__':
-    model = SRCNN(scale=3).compile()
+    model = SRCNN(scale=3)
     dataset = load_datasets('../Data/datasets.json')['91-IMAGE']
     dataset.setattr(patch_size=48, strides=48, depth=1)
     with Environment(model, f'../Results/{model.name}/save', f'../Results/{model.name}/log') as env:
         env.fit(64, 1, dataset, shuffle=False, restart=True)
-    model.recompile(scale=4)
+    # change model with different hyper parameters
+    model = SRCNN(scale=4)
     with Environment(model, f'../Results/{model.name}/save', f'../Results/{model.name}/log') as env:
-        env.fit(64, 5, dataset, shuffle=False, restart=True)
+        env.fit(64, 5, dataset, shuffle=False, restart=False)
         env.output_callbacks = [save_image(f'../Results/{model.name}/test')]
         env.test(dataset)
