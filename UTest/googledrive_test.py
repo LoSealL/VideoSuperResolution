300769e451c2a3d3d5dd46f612afadb2e437187a
tangwy@intel-nuc <twytwy12345@live.com>
2019-01-09 17:35:15 +0800

    [FIX] CI error

--- UTest/googledrive_test.py
+++ UTest/googledrive_test.py
@@ -1,7 +1,10 @@
 from __future__ import print_function
-from googleapiclient.discovery import build
-from httplib2 import Http
-from oauth2client import file, client, tools
+try:
+    from googleapiclient.discovery import build
+    from httplib2 import Http
+    from oauth2client import file, client, tools
+except ImportError:
+    pass
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/drive.readonly'
