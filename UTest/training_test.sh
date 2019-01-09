ea0564e16340ae58eb84e03a3a4968d659232c31
tangwy@intel-nuc <twytwy12345@live.com>
2019-01-09 17:30:45 +0800

    [FIX] CI error

--- UTest/training_test.sh
+++ UTest/training_test.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-pushd ../Train
+pushd ./Train
 
-python run.py --model=srcnn --data_config=./data/fake_datasets.yml --dataset=normal --epochs=1 --test=bar
+python run.py --model=srcnn --data_config=../UTest/data/fake_datasets.yml --dataset=normal --epochs=1 --test=bar
 python run.py --mode=eval --model=srcnn --checkpoint_dir=../Results/srcnn --test=bar --enable_psnr
-python run.py --model=espcn --data_config=./data/fake_datasets.yml --dataset=normal --epochs=1 --test=bar
-python run.py --model=vdsr --data_config=./data/fake_datasets.yml --dataset=normal --epochs=1 --test=bar
-python run.py --model=drcn --data_config=./data/fake_datasets.yml --dataset=normal --epochs=1 --test=bar
-python run.py --model=drrn --data_config=./data/fake_datasets.yml --dataset=normal --epochs=1 --test=bar
+python run.py --model=espcn --data_config=../UTest/data/fake_datasets.yml --dataset=normal --epochs=1 --test=bar
+python run.py --model=vdsr --data_config=../UTest/data/fake_datasets.yml --dataset=normal --epochs=1 --test=bar
+python run.py --model=drcn --data_config=../UTest/data/fake_datasets.yml --dataset=normal --epochs=1 --test=bar
+python run.py --model=drrn --data_config=../UTest/data/fake_datasets.yml --dataset=normal --epochs=1 --test=bar
 popd
