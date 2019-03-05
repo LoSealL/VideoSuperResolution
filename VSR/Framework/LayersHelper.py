[ADD] Default convolution initializer in PyTorch. [FIX] CARN add gradient clip

VSR/Framework/LayersHelper.py
@@ -9,7 +9,7 @@ commonly used layers helper
 import tensorflow as tf
 
 from ..Util.Utility import (SpectralNorm, pixel_shift, pop_dict_wo_keyerror,
-                            prelu, to_list)
+                            prelu, to_list, TorchInitializer)
 
 
 class Layers(object):
@@ -51,12 +51,21 @@ class Layers(object):
              kernel_regularizer='l2', **kwargs):
     """wrap a convolution for common use case"""
 
-    ki, kr = self._kernel(kernel_initializer, kernel_regularizer)
+    if kernel_initializer == 'torch':
+      ki = TorchInitializer()
+      kr = None
+      if use_bias:
+        bi = TorchInitializer(kernel_size * kernel_size * x.shape[-1])
+      else:
+        bi = tf.zeros_initializer()
+    else:
+      ki, kr = self._kernel(kernel_initializer, kernel_regularizer)
+      bi = tf.zeros_initializer()
     nn = tf.layers.Conv2D(filters, kernel_size, strides=strides,
                           padding=padding, data_format=data_format,
                           dilation_rate=dilation_rate, use_bias=use_bias,
                           kernel_initializer=ki, kernel_regularizer=kr,
-                          **kwargs)
+                          bias_initializer=bi, **kwargs)
     nn.build(x.shape.as_list())
     if use_sn:
       nn.kernel = SpectralNorm()(nn.kernel)
@@ -80,12 +89,22 @@ class Layers(object):
              use_in=False, use_ln=False, use_gn=False,
              kernel_initializer='he_normal', kernel_regularizer='l2',
              **kwargs):
-    ki, kr = self._kernel(kernel_initializer, kernel_regularizer)
+
+    if kernel_initializer == 'torch':
+      ki = TorchInitializer()
+      kr = None
+      if use_bias:
+        bi = TorchInitializer(kernel_size * kernel_size * x.shape[-1])
+      else:
+        bi = tf.zeros_initializer()
+    else:
+      ki, kr = self._kernel(kernel_initializer, kernel_regularizer)
+      bi = tf.zeros_initializer()
     nn = tf.layers.Conv3D(filters, kernel_size, strides=strides,
                           padding=padding, data_format=data_format,
                           dilation_rate=dilation_rate, use_bias=use_bias,
                           kernel_initializer=ki, kernel_regularizer=kr,
-                          **kwargs)
+                          bias_initializer=bi, **kwargs)
     nn.build(x.shape.as_list())
     x = nn(x)
     if use_batchnorm:
@@ -119,11 +138,21 @@ class Layers(object):
                **kwargs):
     """warp a conv2d_transpose op for simplicity usage"""
 
-    ki, kr = self._kernel(kernel_initializer, kernel_regularizer)
+    if kernel_initializer == 'torch':
+      ki = TorchInitializer()
+      kr = None
+      if use_bias:
+        bi = TorchInitializer(kernel_size * kernel_size * x.shape[-1])
+      else:
+        bi = tf.zeros_initializer()
+    else:
+      ki, kr = self._kernel(kernel_initializer, kernel_regularizer)
+      bi = tf.zeros_initializer()
     nn = tf.layers.Conv2DTranspose(filters, kernel_size, strides=strides,
                                    padding=padding,
                                    data_format=data_format,
                                    use_bias=use_bias,
+                                   bias_initializer=bi,
                                    kernel_initializer=ki,
                                    kernel_regularizer=kr, **kwargs)
     nn.build(x.shape.as_list())
@@ -158,11 +187,22 @@ class Layers(object):
                kernel_initializer='he_normal',
                kernel_regularizer='l2',
                **kwargs):
-    ki, kr = self._kernel(kernel_initializer, kernel_regularizer)
+
+    if kernel_initializer == 'torch':
+      ki = TorchInitializer()
+      kr = None
+      if use_bias:
+        bi = TorchInitializer(kernel_size * kernel_size * x.shape[-1])
+      else:
+        bi = tf.zeros_initializer()
+    else:
+      ki, kr = self._kernel(kernel_initializer, kernel_regularizer)
+      bi = tf.zeros_initializer()
     nn = tf.layers.Conv3DTranspose(filters, kernel_size, strides=strides,
                                    padding=padding,
                                    data_format=data_format,
                                    use_bias=use_bias,
+                                   bias_initializer=bi,
                                    kernel_initializer=ki,
                                    kernel_regularizer=kr, **kwargs)
     nn.build(x.shape.as_list())
@@ -268,6 +308,9 @@ class Layers(object):
     assert str(method).lower() in _allowed_method
     method = str(method).lower()
     act = kwargs.get('activator')
+    ki = kwargs.get('kernel_initializer', 'he_normal')
+    kr = kwargs.get('kernel_regularizer', 'l2')
+    use_bias = kwargs.get('use_bias', True)
 
     scale_x, scale_y = to_list(scale, 2) or self.scale
     features = self.channel if direct_output else image.shape.as_list()[-1]
@@ -276,8 +319,8 @@ class Layers(object):
         if method == 'espcn':
           image = pixel_shift(self.conv2d(
             image, features * scale_x * scale_y, 3,
-            kernel_initializer='he_normal',
-            kernel_regularizer='l2'), [scale_x, scale_y], features)
+            use_bias=use_bias, kernel_initializer=ki, kernel_regularizer=kr),
+            [scale_x, scale_y], features)
         elif method == 'nearest':
           image = pixel_shift(
             tf.concat([image] * scale_x * scale_y, -1),
@@ -286,7 +329,9 @@ class Layers(object):
         elif method == 'deconv':
           image = self.deconv2d(image, features, 3,
                                 strides=[scale_y, scale_x],
-                                kernel_initializer='he_normal')
+                                kernel_initializer=ki,
+                                kernel_regularizer=kr,
+                                use_bias=use_bias)
         if act:
           image = act(image)
         break
@@ -295,18 +340,17 @@ class Layers(object):
         scale_y //= 2
         if method == 'espcn':
           image = pixel_shift(self.conv2d(
-            image, features * 4, 3,
-            kernel_initializer='he_normal',
-            kernel_regularizer='l2'), [2, 2], features)
+            image, features * 4, 3, use_bias=use_bias,
+            kernel_initializer=ki, kernel_regularizer=kr), [2, 2], features)
         elif method == 'nearest':
           image = pixel_shift(
             tf.concat([image] * 4, -1),
             [2, 2],
             image.shape[-1])
         elif method == 'deconv':
-          image = self.deconv2d(image, features, 3,
-                                strides=2,
-                                kernel_initializer='he_normal')
+          image = self.deconv2d(image, features, 3, strides=2,
+                                use_bias=use_bias,
+                                kernel_initializer=ki, kernel_regularizer=kr)
         if act:
           image = act(image)
     return image

VSR/Models/Carn.py
@@ -14,23 +14,26 @@ from ..Framework.SuperResolution import SuperResolution
 
 
 class CARN(SuperResolution):
-  """Fast, Accurate, and Lightweight Super-Resolution with Cascading Residual Network
+  """Fast, Accurate, and Lightweight Super-Resolution with Cascading Residual
+     Network
 
   Args:
       recursive: A boolean, specifies whether use shared Residual-E weights
       groups: number of groups in group-convolution
       n_residual: number of Residual-E layers in a cascading block
       n_block: number of cascading blocks
+      clip: value of gradient normal clipping
   """
 
   def __init__(self, name='carn', recursive=False, groups=1, n_residual=3,
-               n_blocks=3, filters=64, **kwargs):
+               n_blocks=3, filters=64, clip=10, **kwargs):
     super(CARN, self).__init__(**kwargs)
     self.name = name
     self.recur = recursive
     self.groups = groups
     self.n_residual = n_residual
     self.n_blocks = n_blocks
+    self.clip = clip
     self.F = filters
 
   def _residual_e(self, inputs, reuse=False, **kwargs):
@@ -39,14 +42,17 @@ class CARN(SuperResolution):
       """Fake code since there is no efficient group conv2d in TF!
       see https://github.com/tensorflow/tensorflow/pull/10482
       and https://github.com/tensorflow/tensorflow/issues/3332
-      >>> x = group_conv2d(inputs, self.F, 3, self.groups, activation='relu', kernel_initializer='he_uniform')
-      >>> x = group_conv2d(x, self.F, 3, self.groups, activation='relu', kernel_initializer='he_uniform')
-      >>> x = conv2d(x, inputs.shape[-1], 1, kernel_initializer='he_uniform')
+        >>> x = group_conv2d(inputs, self.F, 3, self.groups, activation='relu',
+                             kernel_initializer='torch')
+        >>> x = group_conv2d(x, self.F, 3, self.groups, activation='relu',
+                             kernel_initializer='torch')
+        >>> x = conv2d(x, inputs.shape[-1], 1,
+                       kernel_initializer='torch')
       """
       # use a normal residual instead
       x = self.conv2d(inputs, self.F, 3, activation='relu',
-                      kernel_initializer='he_uniform')
-      x = self.conv2d(x, inputs.shape[-1], 3, kernel_initializer='he_uniform')
+                      kernel_initializer='torch')
+      x = self.conv2d(x, inputs.shape[-1], 3, kernel_initializer='torch')
       inputs += x
       return tf.nn.relu(inputs)
 
@@ -60,23 +66,24 @@ class CARN(SuperResolution):
         x = self._residual_e(outp_1x1, reuse=self.recur, name=name)
         feat.append(x)
         x = tf.concat(feat, axis=-1)
-        outp_1x1 = self.conv2d(x, F, 1, kernel_initializer='he_uniform')
+        outp_1x1 = self.conv2d(x, F, 1, kernel_initializer='torch')
       return outp_1x1
 
   def build_graph(self):
     super(CARN, self).build_graph()
     with tf.variable_scope(self.name):
       x = self.inputs_preproc[-1] / 255
-      outp_1x1 = self.conv2d(x, self.F, 3, kernel_initializer='he_uniform')
+      outp_1x1 = self.conv2d(x, self.F, 3, kernel_initializer='torch')
       feat = [x]
       F = outp_1x1.shape[-1]
       for i in range(self.n_blocks):
         x = self._cascading(outp_1x1)
         feat.append(x)
         x = tf.concat(feat, axis=-1)
-        outp_1x1 = self.conv2d(x, F, 1, kernel_initializer='he_uniform')
-      sr = self.upscale(outp_1x1, direct_output=False)
-      sr = self.conv2d(sr, self.channel, 3)
+        outp_1x1 = self.conv2d(x, F, 1, kernel_initializer='torch')
+      sr = self.upscale(outp_1x1, direct_output=False,
+                        kernel_initializer='torch')
+      sr = self.conv2d(sr, self.channel, 3, kernel_initializer='torch')
       self.outputs.append(sr * 255)
 
   def build_loss(self):
@@ -88,8 +95,11 @@ class CARN(SuperResolution):
 
       update_op = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
       with tf.control_dependencies(update_op):
-        opt = tf.train.AdamOptimizer(self.learning_rate).minimize(loss,
-                                                                  self.global_steps)
+        opt = tf.train.AdamOptimizer(self.learning_rate)
+        var_n_grad = opt.compute_gradients(loss)
+        grad_clip = tf.contrib.training.clip_gradient_norms(
+          var_n_grad, self.clip)
+        opt = opt.apply_gradients(grad_clip, self.global_steps)
         self.loss.append(opt)
 
     # tensorboard
@@ -106,5 +116,7 @@ class CARN(SuperResolution):
     tf.summary.image('SR', self.outputs[0], 1)
 
   def build_saver(self):
-    self.savers[self.name] = tf.train.Saver(tf.trainable_variables(self.name),
-                                            max_to_keep=1)
+    var_g = tf.global_variables(self.name)
+    misc = tf.global_variables('Loss') + [self.global_steps]
+    self.savers[self.name] = tf.train.Saver(var_g, max_to_keep=1)
+    self.savers['misc'] = tf.train.Saver(misc, max_to_keep=1)